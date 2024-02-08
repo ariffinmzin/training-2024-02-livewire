67acc761ffc6c957f5c34a90f7c19e99659c63cf
Nasrul Hazim Bin Mohamad <nasrulhazim.m@gmail.com>
2024-02-08 14:19:25 +0800

    reorganise per page

--- resources/views/livewire/user-datatable.blade.php
+++ resources/views/livewire/user-datatable.blade.php
@@ -1,14 +1,13 @@
 <div>
-    <div class="py-4 flex justify-between ">
-        <x-input wire:model.live.debounce.500ms="search" class="" placeholder="Search user by name or email..." />
-
+    <div class="py-4 flex justify-around ">
         <select wire:model.live.debounce.500ms="perPage"
-            class="ml-4 border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-gray-500 dark:focus:border-gray-600 focus:ring-gray-500 dark:focus:ring-gray-600 rounded-md shadow-sm">
+            class=" border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-gray-500 dark:focus:border-gray-600 focus:ring-gray-500 dark:focus:ring-gray-600 rounded-md shadow-sm">
             <option value="10">10</option>
             <option value="25">25</option>
             <option value="50">50</option>
             <option value="100">100</option>
         </select>
+        <x-input wire:model.live.debounce.500ms="search" class="ml-4 w-full" placeholder="Search user by name or email..." />
     </div>
 
     <ol>
@@ -20,4 +19,14 @@
         @endforelse
         {{ $users->links() }}
     </ol>
+
+    <div class="py-4 flex justify-around ">
+        <select wire:model.live.debounce.500ms="perPage"
+            class=" border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-gray-500 dark:focus:border-gray-600 focus:ring-gray-500 dark:focus:ring-gray-600 rounded-md shadow-sm">
+            <option value="10">10</option>
+            <option value="25">25</option>
+            <option value="50">50</option>
+            <option value="100">100</option>
+        </select>
+    </div>
 </div>
