4a8b957c5e16cf92cb2a38aa7aaa99427112a3dc
Nasrul Hazim Bin Mohamad <nasrulhazim.m@gmail.com>
2024-02-08 10:31:59 +0800

    Install Jetstream

--- resources/views/livewire/counter.blade.php
+++ resources/views/livewire/counter.blade.php
@@ -1,7 +1,19 @@
 <div>
-    <h1>{{ $count }}</h1>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Counter') }}
+        </h2>
+    </x-slot>
 
-    <button wire:click="tambah">+</button>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg p-8">
+                <h1>{{ $count }}</h1>
 
-    <button wire:click="tolak">-</button>
+                <x-button wire:click="tambah">+</x-button>
+
+                <x-button wire:click="tolak">-</x-button>
+            </div>
+        </div>
+    </div>
 </div>
